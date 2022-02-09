161aba86e05400d34ec9728a8f94a6de41d46cfb
hidarah96 <hidarah96@gmail.com>
2022-02-09 15:54:19 +0700

    Initial Commit

---
+++ Bareksa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bareksa
+//
+//  Created by Rahmad Hidayat on 09/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BareksaTests/BareksaTests.swift
@@ -0,0 +1,33 @@
+//
+//  BareksaTests.swift
+//  BareksaTests
+//
+//  Created by Rahmad Hidayat on 09/02/22.
+//
+
+import XCTest
+@testable import Bareksa
+
+class BareksaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
